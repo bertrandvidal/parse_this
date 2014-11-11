988fa4508b5e2b09d1de24aa8af0f43c6bf1a51a
bertrandvidal <vidal.bertrand@gmail.com>
2014-11-11 17:40:38 -0500

    Pep8-ify code
    Make the code pep8 compliant using autopep8 - except for the blank line between
    the class definition and its docstring I hate that.

--- parse_this.py
+++ parse_this.py
@@ -6,185 +6,186 @@ import sys
 
 
 class ParseThisError(Exception):
-  """Error base class raised by this module."""
-  pass
+    """Error base class raised by this module."""
 
 
 class NoDefault(object):
-  """Use to fill the list of args and default to indicate the argument doesn't
-    have a default value.
-  """
-  pass
+    """Use to fill the list of args and default to indicate the argument doesn't
+      have a default value.
+    """
 
 
 class Self(object):
-  """Special value to use as the type of the self parameter of a method."""
-  pass
+    """Special value to use as the type of the self parameter of a method."""
 
 
 class Class(object):
-  """Special value to use as the type of the class parameter of a classmethod."""
-  pass
+    """Special value to use as the type of the class parameter of a classmethod."""
 
 
 def _get_args_and_defaults(args, defaults):
-  """Return a list of 2-tuples - the argument name and its default value or
-     a special value that indicates there is no default value.
+    """Return a list of 2-tuples - the argument name and its default value or
+       a special value that indicates there is no default value.
 
-  Args:
-    args: list of argument name
-    defaults: tuple of default values
-  """
-  args_and_defaults = []
-  for (k,v) in izip_longest(args[::-1], defaults[::-1], fillvalue=NoDefault):
-    args_and_defaults.append((k,v))
-  return args_and_defaults[::-1]
+    Args:
+      args: list of argument name
+      defaults: tuple of default values
+    """
+    args_and_defaults = []
+    for (k, v) in izip_longest(args[::-1], defaults[::-1], fillvalue=NoDefault):
+        args_and_defaults.append((k, v))
+    return args_and_defaults[::-1]
 
 
 def _prepare_doc(func, args):
-  """From the function docstring get the arg parse description and arguments
-    help message. If there is no docstring simple description and help
-    message are created.
+    """From the function docstring get the arg parse description and arguments
+      help message. If there is no docstring simple description and help
+      message are created.
 
-  Args:
-    func: the function that needs argument parsing
-    args: name of the function arguments
-  """
-  if not func.__doc__:
-    return ("Argument parsing for %s" % func.__name__, ["Help message for %s"
-                                                        % arg for arg in args])
-  description = []
-  args_help = []
-  for line in func.__doc__.split("\n"):
-    if line.strip():
-      description.append(line.strip())
-    else:
-      # The first empty line marks the end of the method description
-      break
-  for argument in args:
-    # TODO: Make this more explicit
-    args_help.append(" ".join([line[line.index(":") + 1:].strip()
-                               for line in func.__doc__.split("\n")
-                               if line.strip().startswith("%s:" % argument)]))
-  return (" ".join(description), args_help)
+    Args:
+      func: the function that needs argument parsing
+      args: name of the function arguments
+    """
+    if not func.__doc__:
+        return ("Argument parsing for %s" % func.__name__,
+                ["Help message for %s" % arg for arg in args])
+    description = []
+    args_help = []
+    for line in func.__doc__.split("\n"):
+        if line.strip():
+            description.append(line.strip())
+        else:
+            # The first empty line marks the end of the method description
+            break
+    for argument in args:
+        # TODO: Make this more explicit
+        args_help.append(" ".join([line[line.index(":") + 1:].strip()
+                                   for line in func.__doc__.split("\n")
+                                   if line.strip().startswith("%s:" % argument)]))
+    return (" ".join(description), args_help)
 
 
 def _get_arg_parser(func, types, args_and_defaults):
-  """Return an ArgumentParser for the given function. Arguments are defined
-    from the function arguments and their associated defaults.
+    """Return an ArgumentParser for the given function. Arguments are defined
+      from the function arguments and their associated defaults.
 
-  Args:
-    func: function for which we want an ArgumentParser
-    types: types to which the command line arguments should be converted to
-    args_and_defaults: list of 2-tuples (arg_name, arg_default)
-  """
-  (description, arg_help) = _prepare_doc(func, [x for (x,y) in args_and_defaults])
-  parser = ArgumentParser(description=description)
-  identity_type = lambda x:x
-  for ((arg, default), arg_type, help_msg) in izip_longest(args_and_defaults, types, arg_help):
-    if default is NoDefault:
-      arg_type = arg_type or identity_type
-      parser.add_argument(arg, help=help_msg, type=arg_type)
-    else:
-      arg_type = arg_type or type(default)
-      parser.add_argument("--%s" % arg, help=help_msg, default=default,
-                          type=arg_type)
-  return parser
+    Args:
+      func: function for which we want an ArgumentParser
+      types: types to which the command line arguments should be converted to
+      args_and_defaults: list of 2-tuples (arg_name, arg_default)
+    """
+    (description, arg_help) = _prepare_doc(
+        func, [x for (x, y) in args_and_defaults])
+    parser = ArgumentParser(description=description)
+    identity_type = lambda x: x
+    for ((arg, default), arg_type, help_msg) in izip_longest(args_and_defaults,
+                                                             types, arg_help):
+        if default is NoDefault:
+            arg_type = arg_type or identity_type
+            parser.add_argument(arg, help=help_msg, type=arg_type)
+        else:
+            arg_type = arg_type or type(default)
+            parser.add_argument("--%s" % arg, help=help_msg, default=default,
+                                type=arg_type)
+    return parser
 
 
 def _get_args_to_parse(args, sys_argv):
-  """Return the given arguments if it is not None else sys.argv if it contains
-    something, an empty list otherwise.
+    """Return the given arguments if it is not None else sys.argv if it contains
+      something, an empty list otherwise.
 
-  Args:
-    args: argument to be parsed
-    sys_argv: arguments of the command line i.e. sys.argv
-  """
-  if args is not None:
-      return args
-  command_line_arguments = sys_argv[1:]
-  if command_line_arguments:
-    return command_line_arguments
-  return []
+    Args:
+      args: argument to be parsed
+      sys_argv: arguments of the command line i.e. sys.argv
+    """
+    if args is not None:
+        return args
+    command_line_arguments = sys_argv[1:]
+    if command_line_arguments:
+        return command_line_arguments
+    return []
 
 
 def _check_types(types, func_args, defaults):
-  """Make sure that enough types were given to ensure conversion. Also remove
-    a potentiel Self arguments.
+    """Make sure that enough types were given to ensure conversion. Also remove
+      a potentiel Self arguments.
 
-  Args:
-    types: a list of Python types to which the argument should be converted to
-    func_args: list of function arguments name
-    defaults: tuple of default values for the function argument
-  Raises:
-    ParseThisError: if the number of types for conversion does not match
-        the number of function's arguments
-  """
-  if len(types) > len(func_args):
-    raise ParseThisError("To many types provided for conversion.")
-  if len(types) < len(func_args) - len(defaults):
-    raise ParseThisError("Not enough types provided for conversion")
-  if types and types[0] in [Self, Class]:
-    types = types[1:]
-    func_args = func_args[1:]
-  return (types, func_args)
+    Args:
+      types: a list of Python types to which the argument should be converted to
+      func_args: list of function arguments name
+      defaults: tuple of default values for the function argument
+    Raises:
+      ParseThisError: if the number of types for conversion does not match
+          the number of function's arguments
+    """
+    if len(types) > len(func_args):
+        raise ParseThisError("To many types provided for conversion.")
+    if len(types) < len(func_args) - len(defaults):
+        raise ParseThisError("Not enough types provided for conversion")
+    if types and types[0] in [Self, Class]:
+        types = types[1:]
+        func_args = func_args[1:]
+    return (types, func_args)
+
 
 def _call(func, func_args, arguments):
-  """Actually calls the function with the arguments parsed from the command line.
+    """Actually calls the function with the arguments parsed from the command line.
 
-  Args:
-    func: the function to called
-    func_args: name of the function arguments
-    arguments: the namespace object parse from the command line
-  """
-  args = []
-  for argument in func_args:
-    args.append(getattr(arguments, argument))
-  return func(*args)
+    Args:
+      func: the function to called
+      func_args: name of the function arguments
+      arguments: the namespace object parse from the command line
+    """
+    args = []
+    for argument in func_args:
+        args.append(getattr(arguments, argument))
+    return func(*args)
 
 
 def parse_this(func, types, args=None):
-  """Create an ArgumentParser for the given function converting the command line
-    arguments according to the list of types.
+    """Create an ArgumentParser for the given function converting the command line
+      arguments according to the list of types.
 
-  Args:
-    func: the function for which the command line arguments to be parsed
-    types: a list of types - as accepted by argparse - that will be used to
-      convert the command line arguments
-    args: a list of arguments to be parsed if None sys.argv is used
-  """
-  (func_args, varargs, keywords, defaults) = getargspec(func)
-  types, func_args =_check_types(types, func_args, defaults)
-  args_and_defaults = _get_args_and_defaults(func_args, defaults)
-  parser = _get_arg_parser(func, types, args_and_defaults)
-  arguments = parser.parse_args(_get_args_to_parse(args, sys.argv))
-  return _call(func, func_args, arguments)
+    Args:
+      func: the function for which the command line arguments to be parsed
+      types: a list of types - as accepted by argparse - that will be used to
+        convert the command line arguments
+      args: a list of arguments to be parsed if None sys.argv is used
+    """
+    (func_args, varargs, keywords, defaults) = getargspec(func)
+    types, func_args = _check_types(types, func_args, defaults)
+    args_and_defaults = _get_args_and_defaults(func_args, defaults)
+    parser = _get_arg_parser(func, types, args_and_defaults)
+    arguments = parser.parse_args(_get_args_to_parse(args, sys.argv))
+    return _call(func, func_args, arguments)
 
 
 class create_parser(object):
-  """Creates an argument parser for the decorated function."""
+    """Creates an argument parser for the decorated function."""
 
-  def __init__(self, *types):
-    """
-    Args:
-      type: vargs list of types to which the command line arguments should be
-        converted to
-    """
-    self.types = types
+    def __init__(self, *types):
+        """
+        Args:
+          type: vargs list of types to which the command line arguments should be
+            converted to
+        """
+        self.types = types
 
-  def __call__(self, func):
-    """Add an argument parser attribute `parser` to the decorated function.
+    def __call__(self, func):
+        """Add an argument parser attribute `parser` to the decorated function.
 
-    Args:
-      func: the function for which we want to create an argument parser
-    """
-    if not hasattr(func, "parser"):
-      (func_args, _, _, defaults) = getargspec(func)
-      self.types, func_args = _check_types(self.types, func_args, defaults)
-      args_and_defaults = _get_args_and_defaults(func_args, defaults)
-      func.parser = _get_arg_parser(func, self.types, args_and_defaults)
-    @wraps(func)
-    def decorated(*args, **kwargs):
-      return func(*args, **kwargs)
-    return decorated
+        Args:
+          func: the function for which we want to create an argument parser
+        """
+        if not hasattr(func, "parser"):
+            (func_args, _, _, defaults) = getargspec(func)
+            self.types, func_args = _check_types(
+                self.types, func_args, defaults)
+            args_and_defaults = _get_args_and_defaults(func_args, defaults)
+            func.parser = _get_arg_parser(func, self.types, args_and_defaults)
+
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            return func(*args, **kwargs)
+        return decorated
 
--- parse_this_test.py
+++ parse_this_test.py
@@ -7,169 +7,175 @@ from parse_this import (_get_args_and_defaults, NoDefault, _get_args_to_parse,
 
 
 def parse_me(one, two, three=12):
-  """Could use some parsing.
-
-  Args:
-    one: some stuff shouldn't be written down
-    two: I can turn 2 syllables words into 6 syllables words
-    three: I don't like the number three
-  """
-  return one * two, three * three
-
-
-def parse_me_no_doc(one, two, three):
-  return one * two, three * three
-
-
-def will_you_dare_parse_me(one, two, three):
-  """I am a sneaky function.
-
-  Args:
-    one: this one is a no brainer
-    three: noticed you're missing docstring for two
-  """
-  return one * two, three * three
-
-
-class TestParseThis(unittest.TestCase):
-
-  def test_get_args_and_default(self):
-    args_and_defaults = _get_args_and_defaults(["first", "second", "third"],
-                                               ("default_value","other_default"))
-    self.assertItemsEqual(args_and_defaults, [("first", NoDefault),
-                                              ("second", "default_value"),
-                                              ("third", "other_default")])
-    self.assertItemsEqual(_get_args_and_defaults([], ()), [])
-
-  def test_get_args_to_parse(self):
-    self.assertItemsEqual(_get_args_to_parse(None, []), [])
-    self.assertItemsEqual(_get_args_to_parse(None, ["prog", "arg"]), ["arg"])
-    self.assertItemsEqual(_get_args_to_parse(None, ["prog", "arg", "--kwargs=12"]),
-                          ["arg", "--kwargs=12"])
-    self.assertItemsEqual(_get_args_to_parse([], []), [])
-    self.assertItemsEqual(_get_args_to_parse(["prog", "arg", "--kwargs=12"], []),
-                                             ["prog", "arg", "--kwargs=12"])
-
-  def test_prepare_doc(self):
-    (description, help_msg) = _prepare_doc(parse_me,
-                                           ["one","two","three"])
-    self.assertEquals(description, "Could use some parsing.")
-    self.assertItemsEqual(help_msg, ["some stuff shouldn't be written down",
-                                     "I can turn 2 syllables words into 6 syllables words",
-                                     "I don't like the number three"])
-
-  def test_prepare_doc_no_docstring(self):
-    (description, help_msg) = _prepare_doc(parse_me_no_doc,
-                                           ["one", "two", "three"])
-    self.assertEquals(description, "Argument parsing for parse_me_no_doc")
-    self.assertItemsEqual(help_msg, ["Help message for one",
-                                     "Help message for two",
-                                     "Help message for three"])
-
-  def test_prepare_doc_will_you_dare(self):
-    (description, help_msg) = _prepare_doc(will_you_dare_parse_me,
-                                           ["one", "two", "three"])
-    self.assertEquals(description, "I am a sneaky function.")
-    self.assertItemsEqual(help_msg, ["this one is a no brainer", "",
-                                     "noticed you're missing docstring for two"])
-
-  def test_check_types(self):
-    self.assertRaises(ParseThisError, _check_types, [], ["arg_one"], ())
-    self.assertRaises(ParseThisError, _check_types, [int, float], ["arg_one"],
-                      ())
-    try:
-      _check_types([], [], ())
-      _check_types([int, int], ["arg_one", "arg_two"], ())
-      _check_types([int, int], ["arg_one", "arg_two"], (12,))
-      _check_types([int], ["arg_one", "arg_two"], (12,))
-      _check_types([Self, int, int], ["self", "arg_one", "arg_two"], ())
-    except Exception, exception:
-      self.fail("_check_types should not have raised: %s" % exception)
-
-  def test_namespace_no_option(self):
-    parser = _get_arg_parser(parse_me, [str, int], [("one", NoDefault),
-                                                    ("two", NoDefault),
-                                                    ("three",12)])
-    namespace = parser.parse_args("yes 42".split())
-    self.assertEquals(namespace.one, "yes")
-    self.assertEquals(namespace.two, 42)
-    self.assertEquals(namespace.three, 12)
-
-  def test_namespace(self):
-    parser = _get_arg_parser(parse_me, [str, int], [("one", NoDefault),
-                                                    ("two", NoDefault),
-                                                    ("three", 12)])
-    namespace =parser.parse_args("no 12 --three=23".split())
-    self.assertEquals(namespace.one, "no")
-    self.assertEquals(namespace.two, 12)
-    self.assertEquals(namespace.three, 23)
-
-  def test_return_value(self):
-    self.assertEquals(parse_this(parse_me, [str, int], "yes 2".split()),
-                      ("yesyes", 144))
-    self.assertEquals(parse_this(parse_me, [str, int], "no 3 --three 2".split()),
-                      ("nonono", 4))
-
-
-
-@create_parser(str, int)
-def iam_parseable(one, two, three=12):
-  """I too want to be parseable.
-
-  Args:
-    one: the one and only
-    two: for the money
-    three: don't like the number three
-  """
-  return one * two, three * three
-
-
-class NeedParsing(object):
-
-  @create_parser(Self, str, int)
-  def could_you_parse_me(self, one, two, three=12):
-    """I would like some arg parsing please.
+    """Could use some parsing.
 
     Args:
-      one: and only one
-      two: will never be first
+      one: some stuff shouldn't be written down
+      two: I can turn 2 syllables words into 6 syllables words
       three: I don't like the number three
     """
     return one * two, three * three
 
-  @classmethod
-  @create_parser(Class, str, int)
-  def parse_me_if_you_can(cls, one, two, three=12):
+
+def parse_me_no_doc(one, two, three):
     return one * two, three * three
+
+
+def will_you_dare_parse_me(one, two, three):
+    """I am a sneaky function.
+
+    Args:
+      one: this one is a no brainer
+      three: noticed you're missing docstring for two
+    """
+    return one * two, three * three
+
+
+class TestParseThis(unittest.TestCase):
+
+    def test_get_args_and_default(self):
+        args_and_defaults = _get_args_and_defaults(["first", "second", "third"],
+                                                   ("default_value",
+                                                    "other_default"))
+        self.assertItemsEqual(args_and_defaults, [("first", NoDefault),
+                                                  ("second", "default_value"),
+                                                  ("third", "other_default")])
+        self.assertItemsEqual(_get_args_and_defaults([], ()), [])
+
+    def test_get_args_to_parse(self):
+        self.assertItemsEqual(_get_args_to_parse(None, []), [])
+        self.assertItemsEqual(
+            _get_args_to_parse(None, ["prog", "arg"]), ["arg"])
+        self.assertItemsEqual(_get_args_to_parse(None, ["prog", "arg",
+                                                        "--kwargs=12"]),
+                              ["arg", "--kwargs=12"])
+        self.assertItemsEqual(_get_args_to_parse([], []), [])
+        self.assertItemsEqual(_get_args_to_parse(["prog", "arg",
+                                                  "--kwargs=12"], []),
+                              ["prog", "arg", "--kwargs=12"])
+
+    def test_prepare_doc(self):
+        (description, help_msg) = _prepare_doc(parse_me,
+                                               ["one", "two", "three"])
+        self.assertEquals(description, "Could use some parsing.")
+        self.assertItemsEqual(help_msg, ["some stuff shouldn't be written down",
+                                         "I can turn 2 syllables words into 6 syllables words",
+                                         "I don't like the number three"])
+
+    def test_prepare_doc_no_docstring(self):
+        (description, help_msg) = _prepare_doc(parse_me_no_doc,
+                                               ["one", "two", "three"])
+        self.assertEquals(description, "Argument parsing for parse_me_no_doc")
+        self.assertItemsEqual(help_msg, ["Help message for one",
+                                         "Help message for two",
+                                         "Help message for three"])
+
+    def test_prepare_doc_will_you_dare(self):
+        (description, help_msg) = _prepare_doc(will_you_dare_parse_me,
+                                               ["one", "two", "three"])
+        self.assertEquals(description, "I am a sneaky function.")
+        self.assertItemsEqual(help_msg, ["this one is a no brainer", "",
+                                         "noticed you're missing docstring for two"])
+
+    def test_check_types(self):
+        self.assertRaises(ParseThisError, _check_types, [], ["arg_one"], ())
+        self.assertRaises(ParseThisError, _check_types, [int, float],
+                          ["arg_one"], ())
+        try:
+            _check_types([], [], ())
+            _check_types([int, int], ["arg_one", "arg_two"], ())
+            _check_types([int, int], ["arg_one", "arg_two"], (12,))
+            _check_types([int], ["arg_one", "arg_two"], (12,))
+            _check_types([Self, int, int], ["self", "arg_one", "arg_two"], ())
+        except Exception, exception:
+            self.fail("_check_types should not have raised: %s" % exception)
+
+    def test_namespace_no_option(self):
+        parser = _get_arg_parser(parse_me, [str, int], [("one", NoDefault),
+                                                        ("two", NoDefault),
+                                                        ("three", 12)])
+        namespace = parser.parse_args("yes 42".split())
+        self.assertEquals(namespace.one, "yes")
+        self.assertEquals(namespace.two, 42)
+        self.assertEquals(namespace.three, 12)
+
+    def test_namespace(self):
+        parser = _get_arg_parser(parse_me, [str, int], [("one", NoDefault),
+                                                        ("two", NoDefault),
+                                                        ("three", 12)])
+        namespace = parser.parse_args("no 12 --three=23".split())
+        self.assertEquals(namespace.one, "no")
+        self.assertEquals(namespace.two, 12)
+        self.assertEquals(namespace.three, 23)
+
+    def test_return_value(self):
+        self.assertEquals(parse_this(parse_me, [str, int], "yes 2".split()),
+                          ("yesyes", 144))
+        self.assertEquals(parse_this(parse_me, [str, int],
+                                     "no 3 --three 2".split()),
+                          ("nonono", 4))
+
+
+@create_parser(str, int)
+def iam_parseable(one, two, three=12):
+    """I too want to be parseable.
+
+    Args:
+      one: the one and only
+      two: for the money
+      three: don't like the number three
+    """
+    return one * two, three * three
+
+
+class NeedParsing(object):
+
+    @create_parser(Self, str, int)
+    def could_you_parse_me(self, one, two, three=12):
+        """I would like some arg parsing please.
+
+        Args:
+          one: and only one
+          two: will never be first
+          three: I don't like the number three
+        """
+        return one * two, three * three
+
+    @classmethod
+    @create_parser(Class, str, int)
+    def parse_me_if_you_can(cls, one, two, three=12):
+        return one * two, three * three
 
 
 class TestParseable(unittest.TestCase):
 
-  def test_parseable(self):
-    parser = iam_parseable.parser
-    namespace = parser.parse_args("yes 2 --three 3".split())
-    self.assertEquals(namespace.one, "yes")
-    self.assertEquals(namespace.two, 2)
-    self.assertEquals(namespace.three, 3)
-    self.assertEquals(iam_parseable("yes", 2, 3), ("yesyes", 9))
+    def test_parseable(self):
+        parser = iam_parseable.parser
+        namespace = parser.parse_args("yes 2 --three 3".split())
+        self.assertEquals(namespace.one, "yes")
+        self.assertEquals(namespace.two, 2)
+        self.assertEquals(namespace.three, 3)
+        self.assertEquals(iam_parseable("yes", 2, 3), ("yesyes", 9))
 
-  def test_parseable_method(self):
-    need_parsing = NeedParsing()
-    parser = need_parsing.could_you_parse_me.parser
-    namespace = parser.parse_args("yes 2 --three 3".split())
-    self.assertEquals(namespace.one, "yes")
-    self.assertEquals(namespace.two, 2)
-    self.assertEquals(namespace.three, 3)
-    self.assertEquals(need_parsing.could_you_parse_me("yes", 2, 3), ("yesyes", 9))
+    def test_parseable_method(self):
+        need_parsing = NeedParsing()
+        parser = need_parsing.could_you_parse_me.parser
+        namespace = parser.parse_args("yes 2 --three 3".split())
+        self.assertEquals(namespace.one, "yes")
+        self.assertEquals(namespace.two, 2)
+        self.assertEquals(namespace.three, 3)
+        self.assertEquals(
+            need_parsing.could_you_parse_me("yes", 2, 3), ("yesyes", 9))
 
-  def test_parseable_class(self):
-    parser = NeedParsing.parse_me_if_you_can.parser
-    namespace = parser.parse_args("yes 2 --three 3".split())
-    self.assertEquals(namespace.one, "yes")
-    self.assertEquals(namespace.two, 2)
-    self.assertEquals(namespace.three, 3)
-    self.assertEquals(NeedParsing.parse_me_if_you_can("yes", 2, 3), ("yesyes", 9))
+    def test_parseable_class(self):
+        parser = NeedParsing.parse_me_if_you_can.parser
+        namespace = parser.parse_args("yes 2 --three 3".split())
+        self.assertEquals(namespace.one, "yes")
+        self.assertEquals(namespace.two, 2)
+        self.assertEquals(namespace.three, 3)
+        self.assertEquals(
+            NeedParsing.parse_me_if_you_can("yes", 2, 3), ("yesyes", 9))
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
