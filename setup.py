59e1062048b5df128869cdc32c989eac54b75819
bertrandvidal <vidal.bertrand@gmail.com>
2014-11-11 20:00:16 -0500

    Add setup files
    The setup files allows to create a distribution package, upload it to PyPI and
    run tests.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name = "parse_this",
+    version = "0.1",
+    description = "Makes it easy to parse command line arguments for any function, method or classmethod..",
+    long_description = readme,
+    author = "Bertrand Vidal",
+    author_email = "vidal.bertrand@gmail.com",
+    download_url = "https://pypi.python.org/pypi/parse_this",
+    url = "https://github.com/bertrandvidal/parse_this",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    setup_requires = [
+        "nose",
+    ],
+)
