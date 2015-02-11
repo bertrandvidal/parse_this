1557e27ccc9902edad3ee4ee7c067d1c824d190e
bertrandvidal <vidal.bertrand@gmail.com>
2015-02-11 19:32:20 +0100

    Bump version to 1.0.0
    The introduction to the method 'call' !

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst'), "r") as readme_
 
 setup(
     name = "parse_this",
-    version = "0.4.0",
+    version = "1.0.0",
     description = "Makes it easy to create a command line interface for any function, method or classmethod..",
     long_description = readme,
     py_modules = ["parse_this", "parse_this_test"],
