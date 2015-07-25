3311debac39963b5d413ec3897856fbe066ed833
bertrandvidal <vidal.bertrand@gmail.com>
2015-07-26 01:03:38 +0200

    Add a script to run test for py2.7 and py3.4
    To make it easier to run test for both versions

---
+++ test_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# test in python 2.7
+function test_python_version(){
+  ENV_PATH=$1
+  PY_VERSION=$2
+  if [ ! -d ${ENV_PATH} ]; then
+    virtualenv --prompt="(parse_this_${PY_VERSION})" --python=python${PY_VERSION} ${ENV_PATH}
+  fi
+  source ${ENV_PATH}/bin/activate
+  python setup.py develop
+  python setup.py nosetests
+}
+
+test_python_version "env_27" "2.7"
+test_python_version "env_34" "3.4"
