f23d22887b6756b1c8624aeb4732281f6b7c3229
bertrandvidal <vidal.bertrand@gmail.com>
2022-01-23 17:52:40 -0500

    Increment version to avoid pypi failing on same upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(README_PATH, "r") as README_FILE:
 
 setup(
     name="parse_this",
-    version="2.0.0",
+    version="2.0.1",
     description=(
         "Makes it easy to create a command line interface for any "
         "function, method or classmethod.."
